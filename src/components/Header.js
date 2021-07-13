8690f2c7ecd8b22f5ab408be334d0e7323829529
bra-Ben <74235145+bra-Ben@users.noreply.github.com>
2021-07-13 09:05:27 +0000

    slider completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,6 +50,7 @@ const Nav = styled.nav`
     display:    flex;
     align-items:    center;
     padding:    0 36px;
+    overflow: hidden;
 
 `
 
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -16,7 +16,13 @@ function ImageSlider() {
 
     return (
         <Carousel {...settings}>
-            
+            <Wrap>
+                <img src='/images/slider-badging.jpg' />
+            </Wrap>
+            <Wrap>
+                <img src='/images/slider-badag.jpg' />
+            </Wrap>
+
         </Carousel>
     )
 }
@@ -24,9 +30,49 @@ function ImageSlider() {
 export default ImageSlider
 
 const Carousel = styled(Slider)`
+    margin-top: 20px;
 
+    ul li button {
+        &:before {
+            font-size: 10px;
+            color: rgb(150, 150, 171);
+        }
+    }
+
+    li:slick-active button:before {
+        color: white;
+    }
+
+    .slick-list {
+        overflow: visible;
+    }
+
+    button {
+        z-index: 1;
+
+    }
 `
 
+const Wrap = styled.div`
+    cursor: pointer;
+    
+    img {
+        border: 4px solid transparent;
+        border-radius: 5px;
+        width: 100%;
+        height: 100%;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        transition-duration: 300ms;
+        
+
+        &:hover {
+            border: 4px solid rgba(249, 249, 249, 0.8);
+ 
+        }
+    }
+
+`
 
 
 

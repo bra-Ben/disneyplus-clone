b0d8a3f05094d6201d95852594f9b8c1fecabd44
bra-Ben <74235145+bra-Ben@users.noreply.github.com>
2021-07-16 21:49:39 +0000

    Detail component styling

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -5,7 +5,9 @@ import styled from "styled-components"
 function Detail() {
     return (
         <Container>
-            Detail
+            <Background>
+                <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg' />
+            </Background>
 
 
 
@@ -20,6 +22,24 @@ export default Detail
 
 const Container = styled.div`
     min-height: calc(100vh - 70px);
-    padding: 0 calc(3.5vw + 5px)
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+`
+
+const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
 
 `
+
+
+
